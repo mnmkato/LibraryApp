d6b27f04ebbb5d89c730cde5eed972568b59e39b
mnmkato <katsmamal@gmail.com>
2024-04-30 16:52:49 +0000

    set up functionality for delete and some updates

--- express-library/models/author.js
+++ express-library/models/author.js
@@ -3,10 +3,10 @@ const { DateTime } = require("luxon");
 
 const Schema = mongoose.Schema;
 const AuthorSchema = new Schema({
-first_name: {type : String, required: true, maxlength: 100}, 
-family_name: {type : String, required: true, maxlength: 100}, 
-date_of_birth: {type : Date}, 
-date_of_death: {type : Date},
+  first_name: {type : String, required: true, maxlength: 100}, 
+  family_name: {type : String, required: true, maxlength: 100}, 
+  date_of_birth: {type : Date}, 
+  date_of_death: {type : Date},
 })
 
 // Virtual for author's full name
